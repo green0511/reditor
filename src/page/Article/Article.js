883b9320099120f524a79661d681cec6612a45b2
green0511 <linhao_me@163.com>
2019-10-13 22:13:43 +0800

    feat: 完善文章创建流程

--- src/page/Article/Article.js
+++ src/page/Article/Article.js
@@ -1,15 +1,24 @@
 import React from 'react';
-
-import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
 
 import style from './style.module.css';
 
-export default () => {
+import { getFormtedDate } from '../../utils/util';
+
+const Article = (props) => {
   const history = useHistory();
+  const routeParams = useParams();
+  const articleId = routeParams.id;
+  const { list, searchMap } = props.notes || {};
+  const articleIndex = searchMap[articleId];
+  const articleData = list[articleIndex];
+  console.log(articleData);
 
   return (
     <div>
-      <header className={style.topbar}>
+      <header className={style.topbar} style={{ backgroundImage: `url(${articleData.cover})` }}>
+        <div className={style.topbarMask} />
         <i 
           onClick={() => history.goBack()}
           className={`iconfont icon-jiantou-px ${style.topbarBackIcon}`}
@@ -21,16 +30,24 @@ export default () => {
           EDIT
         </span>
       </header>
-      <div className={style.draftTip}>该笔记有暂未发布的草稿，点击编辑继续创作~</div>
+      {
+        articleData.last_edited_content ? (
+          <div className={style.draftTip}>该笔记有暂未发布的草稿，点击编辑继续创作~</div>
+        ) : null
+      }
       <div className={style.articleContainer}>
-        <h1 className={style.title}>This is the title</h1>
-        <p className={style.time}>FEB 19, 2019</p>
+        <h1 className={style.title}>{articleData.title}</h1>
+        <p className={style.time}>{getFormtedDate(new Date(articleData.created_at))}</p>
         <div className={style.splitLine}></div>
-        <article className={style.article}>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet incidunt eligendi sit possimus blanditiis totam dolorem quod animi assumenda ea labore soluta itaque ex, aperiam veritatis eos ad commodi nobis.</p>
-          <p><strong>Lorem</strong> ipsum dolor sit amet consectetur adipisicing elit. Amet incidunt eligendi sit possimus blanditiis totam dolorem quod animi assumenda ea labore soluta itaque ex, aperiam veritatis eos ad commodi nobis.</p>
-        </article>
+        <article className={style.article} dangerouslySetInnerHTML={{ __html: articleData.content }}></article>
       </div>
     </div>
   );
 };
+
+
+const mapState = state => ({
+  ...state,
+});
+
+export default connect(mapState)(Article);
--- src/store/notes.js
+++ src/store/notes.js
@@ -1,36 +1,26 @@
-// const mockData = [
-//   {
-//     create_at: new Date(2017, 8, 17).getTime(), title: '当我了解你只活在记忆里头当我了解你只活在记忆里头当我了解你只活在记忆里头', summary: '如果你爱着我,而我也爱着你,结果还是有段跨不过的距离',
-//   },
-//   {
-//     create_at: new Date(2017, 8, 17).getTime(), title: '当我了解你只活在记忆里头', summary: '如果你爱着我,而我也爱着你,结果还是有段跨不过的距离如果你爱着我,而我也爱着你,结果还是有段跨不过的距离如果你爱着我,而我也爱着你,结果还是有段跨不过的距离',
-//   },
-//   {
-//     create_at: new Date(2017, 8, 17).getTime(), title: '当我了解你只活在记忆里头', summary: '如果你爱着我,而我也爱着你,结果还是有段跨不过的距离',
-//   },
-//   {
-//     create_at: new Date(2017, 8, 17).getTime(), title: '当我了解你只活在记忆里头', summary: '如果你爱着我,而我也爱着你,结果还是有段跨不过的距离',
-//   },
-//   {
-//     create_at: new Date(2017, 8, 17).getTime(), title: '当我了解你只活在记忆里头', summary: '如果你爱着我,而我也爱着你,结果还是有段跨不过的距离',
-//   },
-//   {
-//     create_at: new Date(2017, 8, 17).getTime(), title: '当我了解你只活在记忆里头', summary: '如果你爱着我,而我也爱着你,结果还是有段跨不过的距离',
-//   },
-//   {
-//     create_at: new Date(2017, 8, 17).getTime(), title: '当我了解你只活在记忆里头', summary: '如果你爱着我,而我也爱着你,结果还是有段跨不过的距离',
-//   },
-//   {
-//     create_at: new Date(2017, 8, 17).getTime(), title: '当我了解你只活在记忆里头', summary: '如果你爱着我,而我也爱着你,结果还是有段跨不过的距离',
-//   },
-//   {
-//     create_at: new Date(2017, 10, 17).getTime(), title: '当你离开的时候',  summary: '如果你爱着我,而我也爱着你,结果还是有段跨不过的距离',
-//   }
-// ];
+import { getRandomId, getRandomCover, getFirstNote } from '../utils/util';
+import storage from '../utils/storage';
+
+const LS_KEY_NOTES = 'LS_KEY_NOTES';
+const cacheList = storage.getItem(LS_KEY_NOTES);
+
+const initList = cacheList || [ getFirstNote() ];
+
+// 生成 id => index 的映射，便于根据 id 查找对应的 item 数据
+const generateSearchMapFromList = (list) => {
+  const result = {};
+  list.forEach((item, index) => {
+    result[item._id] = index;
+  });
+  return result;
+};
+
+storage.setItem(LS_KEY_NOTES, initList);
 
 export default {
   state: {
-    list: [],
+    list: initList,
+    searchMap: generateSearchMapFromList(initList),
   },
   reducers: {
     addNote(state, payload) {
@@ -41,17 +31,38 @@ export default {
           payload,
         ],
       }
+    },
+    setSearchMap(state, payload = {}) {
+      return {
+        ...state,
+        searchMap: payload,
+      };
     }
   },
   effects: (dispatch) => ({
     submit(payload) {
       const { title = '无标题', content = '', summary = '' } = payload;
       dispatch.notes.addNote({
-        create_at: Date.now(),
+        _id: getRandomId(32, 16),
+        created_at: Date.now(),
+        updated_at: Date.now(),
         title,
         content,
-        summary: summary
+        summary: summary,
+        cover: getRandomCover(),
+        status: 1,
+        last_edited_content: '',
       });
+      dispatch.notes.refreshSearchMap();
+      dispatch.notes.saveToLs();
+    },
+    refreshSearchMap(payload, rootState) {
+      const searchMap = generateSearchMapFromList(rootState.notes.list);
+      console.log(searchMap);
+      dispatch.notes.setSearchMap(searchMap);
+    },
+    saveToLs(payload, rootState) {
+      storage.setItem(LS_KEY_NOTES, rootState.notes.list);
     }
   })
 }
