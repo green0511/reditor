d7b76336b9defb2939a62f9d92b7510018bbcca9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-14 13:09:22 +0000

    Deploy green0511/reditor to github.com/green0511/reditor.git:gh-pages

---
+++ precache-manifest.404a75e0b7145657f003dc39954eeff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529ce56495fb07c5345f8e1481e451e0",
+    "url": "/reditor/index.html"
+  },
+  {
+    "revision": "55907beb75d1772629fb",
+    "url": "/reditor/static/css/2.eca02e85.chunk.css"
+  },
+  {
+    "revision": "a740dd8a0a155ee526b3",
+    "url": "/reditor/static/css/main.afffe533.chunk.css"
+  },
+  {
+    "revision": "55907beb75d1772629fb",
+    "url": "/reditor/static/js/2.d24b0913.chunk.js"
+  },
+  {
+    "revision": "a740dd8a0a155ee526b3",
+    "url": "/reditor/static/js/main.55822e93.chunk.js"
+  },
+  {
+    "revision": "77870eab2a1095cf3b5a",
+    "url": "/reditor/static/js/runtime-main.d16b30a0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7dbc69cda80934db5304b648b0b1fdba.js"
+  "/reditor/precache-manifest.404a75e0b7145657f003dc39954eeff7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reditor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
